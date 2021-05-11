6a54e21b28f22bd70ea079b35a2365cf87a46615
Youngteac Hong <youngteac.hong@navercorp.com>
2021-05-11 16:22:42 +0900

    Add creation time to project API

--- api/converter/to_pb.go
+++ api/converter/to_pb.go
@@ -1,21 +1,18 @@
 package converter
 
 import (
+	"google.golang.org/protobuf/types/known/timestamppb"
+
 	pb "oss.navercorp.com/metis/metis-server/api"
 	"oss.navercorp.com/metis/metis-server/server/database"
 )
 
 // ToProject converts the given model to Protobuf message.
 func ToProject(project *database.Project) *pb.Project {
-	// pbCreatedAt, err := ptypes.TimestampProto(project.CreatedAt)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
 	return &pb.Project{
-		Id:   project.ID.String(),
-		Name: project.Name,
-		// CreatedAt: pbCreatedAt,
+		Id:        project.ID.String(),
+		Name:      project.Name,
+		CreatedAt: timestamppb.New(project.CreatedAt),
 	}
 }
 
